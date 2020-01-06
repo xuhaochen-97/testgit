454eb17a1c2dd03f12eae381cb74d2f1b52de6bd
xuhaochen <752203047@qq.com>
2020-01-06 17:28:18 +0800

    第二次提交

---
+++ test.java
@@ -0,0 +1,11 @@
+package testgit;
+
+public class test {
+    public static void main(String[] args) {
+        int a=1;
+        int b=2;
+        int c=a+b;
+        System.out.println(c);
+    }
+
+}
